69ef1de7dbbc23bd1c6c138fab26cef7ae24f4e5
bala-amypo <bala@amypo.in>
2025-12-18 02:41:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -9,44 +9,44 @@ import jakarta.persistence.Id;
 public class StudentEntity {
 
     @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-            private Long id;
-                private String name;
-                    private String email;
-                        private float cgpa;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private float cgpa;
 
-                            public StudentEntity(Long id, String name, String email, float cgpa) {
-                                    this.id = id;
-                                            this.name = name;
-                                                    this.email = email;
-                                                            this.cgpa = cgpa;
-                                                                }
+    public StudentEntity(Long id, String name, String email, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cgpa = cgpa;
+    }
 
-                                                                    public StudentEntity() {
-                                                                            
-                                                                                }
+    public StudentEntity() {
+        
+    }
 
-                                                                                    public void setName(String name) {
-                                                                                            this.name = name;
-                                                                                                }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-                                                                                                    public String getName() {
-                                                                                                            return this.name;
-                                                                                                                }
+    public String getName() {
+        return this.name;
+    }
 
-                                                                                                                    public void setEmail(String email) {
-                                                                                                                            this.email = email;
-                                                                                                                                }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-                                                                                                                                    public String getEmail() {
-                                                                                                                                            return this.email;
-                                                                                                                                                }
+    public String getEmail() {
+        return this.email;
+    }
 
-                                                                                                                                                    public void setCgpa(float cgpa) {
-                                                                                                                                                            this.cgpa = cgpa;
-                                                                                                                                                                }
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
 
-                                                                                                                                                                    public float getCgpa() {
-                                                                                                                                                                            return this.cgpa;
-                                                                                                                                                                                }
-                                                                                                                                                                                }
+    public float getCgpa() {
+        return this.cgpa;
+    }
+}
